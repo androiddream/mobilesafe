c45b465aa0526825ced9da367b4142fc76791c7c
blankfragment <245010735@qq.com>
2015-08-24 23:48:34 +0800

    first commit

---
+++ app/src/main/java/com/example/leemobilesafe/test/TestCreateDB.java
@@ -0,0 +1,24 @@
+package com.example.leemobilesafe.test;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import com.example.leemobilesafe.db.BlackNumberDBOpenHelper;
+
+/**
+ * Created by sing on 14-1-17.
+ * desc:
+ */
+public class TestCreateDB extends AndroidTestCase {
+    private static final String TAG = "TestCreateDB";
+
+    public void test() throws Exception {
+        BlackNumberDBOpenHelper helper = new BlackNumberDBOpenHelper(getContext());
+        SQLiteDatabase db = helper.getWritableDatabase();
+        if (db.isOpen()) {
+            Log.i(TAG, "ok");
+            db.close();
+        }
+    }
+}
